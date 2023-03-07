611fded7fae55f0635dd4923925e7a8691100981
Jeff <rjhughes42@gmail.com>
2023-03-07 08:25:29 -0500

    edited css and error functionality

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-//jshint esversion: 6
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+const https = require("https");
 
-const express = require("express")
-const body = require("body-parser")
-const request = require("request")
-const app = express()
-const https = require("https")
-
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
-    res.sendFile(__dirname + "/signup.html")
-})
+    res.sendFile(__dirname + "/signup.html");
+});
 
-app.use(body.urlencoded({extended:true}))
-app.use(express.static("public"))
-
-app.post("/", function(req,res){
-    const email =  req.body.email
-    const firstName = req.body.firstName
-    const lastName = req.body.lastName
+app.post("/submit", function(req,res){
+    console.log("submit being")
+    const email =  req.body.email;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
     const data = {
         members:[
             {
@@ -29,28 +27,37 @@ app.post("/", function(req,res){
             }
         }
     ]
-    }
-    const jsonData = JSON.stringify(data)
+    };
+    const jsonData = JSON.stringify(data);
     const url = "https://us18.api.mailchimp.com/3.0/lists/bc81d2c761"
     const options = {
         method:"POST",
         auth:"jeff:a26fe1a13d6b0529e66a3d635cf18fc2-us18"
-    }
+    };
     const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+           res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
         response.on("data", function(data){
             console.log(JSON.parse(data))
-        })
-    })
+        });
+    });
+    console.log(jsonData)
     request.write(jsonData)
-    request.end();
+    console.log(jsonData)
+
+    request.end()
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/")
 })
 
-
-
-app.listen(3000, function(){
-    console.log("server running port 3000")
-})
-
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server running port 3000");
+});
 
 // api: a26fe1a13d6b0529e66a3d635cf18fc2-us18
 
